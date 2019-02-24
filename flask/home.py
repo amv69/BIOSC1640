b453bf64339a55e4a6281ea3d840df2f3b4ba8d4
Viz <37909652+alexvisbisky@users.noreply.github.com>
2019-02-24 02:09:44 -0500

    Create home.py

---
+++ flask/home.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from forms import Parameters
+export PYTHONPATH=/BIOSC1640/flask/backEnd/python
+import Oligo
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '051259712ab12de981acb'
+
+data = [
+    {
+        'fastA': 'ATCG',
+        'lengthOne': '5',
+        'lengthTwo': '10',
+        'tempOne': '20',
+        'tempTwo': '40',
+    }
+
+]
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template('home.html', data=data)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/submit", methods=['GET', 'POST'])
+def submit():
+    form = Parameters()
+    if form.validate_on_submit()
+        flash(f'Job Started!')
+    return render_template('submit.html', title='Submit', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
