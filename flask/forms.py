10ae75e9da9b665e6cb6b2f9b2233ed32f56d87f
Viz <37909652+alexvisbisky@users.noreply.github.com>
2019-02-24 20:40:54 -0500

    QOL

--- flask/forms.py
+++ flask/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SubmitField, StringField
 from wtforms.validators import DataRequired, Optional
+from wtforms.widgets import TextArea
 
 class Parameters(FlaskForm):
-    fastA = StringField('FastA', validators=[DataRequired()])
+    fastA = StringField('FastA', validators=[DataRequired()], widget=TextArea())
     lengthOne = IntegerField('Length Minimum', validators=[DataRequired()])
     lengthTwo = IntegerField('Length Maximum', validators=[DataRequired()])
     tempOne = IntegerField('Temperature Minimum', validators=[DataRequired()])
