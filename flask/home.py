b139d7ad69ca629b4888c49f2dcdb218b307df61
Viz <37909652+alexvisbisky@users.noreply.github.com>
2019-02-27 01:41:50 -0500

    Testing OUtput

--- flask/home.py
+++ flask/home.py
@@ -33,19 +33,9 @@ def about():
 def submit():
     form = Parameters()
     if form.validate_on_submit():
-        def inner():
-            proc = subprocess.Popen(
-            pretty_print_oligos({form.fastA.data}, tile_oligos_with_gaps({form.fastA.data}, min_len = {form.lengthOne.data}, max_len = {form.lengthTwo.data}, min_tm={form.tempOne.data}, max_tm={form.tempTwo.data}, max_untiled_len = 25)),             #call something with a lot of output so we can see it
-            shell=True,
-            stdout=subprocess.PIPE
-        )
-
-        for line in iter(proc.stdout.readline,''):
-            time.sleep(1)                           # Don't need this just shows the text streaming
-            yield line.rstrip() + '<br/>\n'
-
-    return flask.Response(inner(), mimetype='text/html')
-        
+        output = execute('./oligo.py')
+        return render_template('submit.html',output=output)
+        #{form.output.data} = str((pretty_print_oligos({form.fastA.data}, tile_oligos_with_gaps({form.fastA.data}, min_len = {form.lengthOne.data}, max_len = {form.lengthTwo.data}, min_tm={form.tempOne.data}, max_tm={form.tempTwo.data}, max_untiled_len = 25))))
     return render_template('submit.html', title='Submit', form=form)
 
 @app.route("/cite")
