55fe4f927f016c7efc45fcdd380e2ba9a759dacf
Viz <37909652+alexvisbisky@users.noreply.github.com>
2019-02-27 02:21:10 -0500

    Update home.py

--- flask/home.py
+++ flask/home.py
@@ -4,7 +4,6 @@ import bioutil
 import math
 import time
 import subprocess
-from io import StringIO
 
 app = Flask(__name__)
 
@@ -720,13 +719,12 @@ def pretty_print_oligos(seq, oligos):
     """
     Also count number of wildcard positions, expansions, oligo seq
     """
-    print('#Target sequence: %d nts' % (len(seq)))
-    print('\t'.join(['Start', 'End', 'Length', 'Tm_low', 'Tm_high', 'X_pos', 'Ambig_pos', 'Num_targets', 'Target_seq', 'Antisense_oligo']))
+    
     for oligo in oligos[1:-1]:
         subseq = seq[oligo[0]:oligo[1]]
         x_count, wc_count, expand_count = wildcard_stats(subseq)
-        print ('\t'.join([str(oligo[0]+1), str(oligo[1]), str(oligo[1]-oligo[0]), str(oligo[2]), str(oligo[3]), str(x_count), str(wc_count), str(expand_count), subseq, bioutil.rc(subseq)]))
-
+        my_list.append('\t'.join([str(oligo[0]+1), str(oligo[1]), str(oligo[1]-oligo[0]), str(oligo[2]), str(oligo[3]), str(x_count), str(wc_count), str(expand_count), subseq, bioutil.rc(subseq)]))
+    return str(('#Target sequence: %d nts' % (len(seq))) + ('\t'.join(['Start', 'End', 'Length', 'Tm_low', 'Tm_high', 'X_pos', 'Ambig_pos', 'Num_targets', 'Target_seq', 'Antisense_oligo'])) + my_list
     
 seq = 'GACTCTTAGCRGYGGATXACTCGGCTCGTGCGTCGATGAAGAACGCAGCTAGCTGCGAGAATTAATGTGAATTGCAGGACACATTGATCATCGACACTTCGAACGCACTTGCGGCCCCGGGTTCCTCCCGGGGCTACGCCTGTCTGAGCGTCGGTTG'
 #pretty_print_oligos(seq,tile_oligos_with_gaps(seq, min_len = 40, max_len = 50, min_tm=70, max_tm=80,max_untiled_len = 25)) #mouse 5.8S
